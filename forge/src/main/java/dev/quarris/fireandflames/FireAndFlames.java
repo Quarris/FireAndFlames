9fa7d9162fac9f4e1a1f52955d8713ba55bd7e7f
Quarris <tomasz.winnicki@live.co.uk>
2025-04-13 16:57:05 +0100

    Initial Commit

---
+++ forge/src/main/java/dev/quarris/fireandflames/FireAndFlames.java
@@ -0,0 +1,18 @@
+package dev.quarris.fireandflames;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class FireAndFlames {
+
+    public FireAndFlames() {
+        // This method is invoked by the Forge mod loader when it is ready
+        // to load your mod. You can access Forge and Common code in this
+        // project.
+
+        // Use Forge to bootstrap the Common mod.
+        Constants.LOG.info("Hello Forge world!");
+        CommonClass.init();
+
+    }
+}
